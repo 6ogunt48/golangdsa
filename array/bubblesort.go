ff77337b987a9280af9118c94ac7ee22032cc88e
6ogunt48 <114788338+6ogunt48@users.noreply.github.com>
2023-12-29 22:53:26 +0000

    added bubble sort to array

---
+++ array/bubblesort.go
@@ -0,0 +1,29 @@
+package array
+
+import "fmt"
+
+// BubbleSortAsc sorts an array using the bubble sort algorithm
+func BubbleSortAsc(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		// swapped to check if any swap happened in this pass
+		swapped := false
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				// swap elements if they are in the wrong order
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = true
+			}
+		}
+		// if no two elements were swapped, the array is sorted
+		if !swapped {
+			break
+		}
+	}
+}
+
+func main() {
+	arr := []int{64, 32, 25, 12, 22, 11, 90}
+	BubbleSortAsc(arr)
+	fmt.Printf("Sorted array: %v\n", arr)
+}
