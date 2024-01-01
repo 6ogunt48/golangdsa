933621b4c8fe246a06e3821aac5370ec3bac3eff
6ogunt48 <114788338+6ogunt48@users.noreply.github.com>
2024-01-01 21:08:45 +0000

    added palindrome

---
+++ bigo/palindromechecker.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	runes := []rune(s)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		if runes[i] != runes[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	testStrings := []string{"radar", "madam", "hello"}
+	for _, str := range testStrings {
+		fmt.Printf("Is '%s' a palindrome? %t\n", str, isPalindrome(str))
+	}
+}
